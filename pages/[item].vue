7143b677cfad0d34ae45bb731c3c36a534720600
korbal <balint.j.korosi@gmail.com>
2022-11-22 13:01:03 +0100

    'fasz'

--- pages/[item].vue
+++ pages/[item].vue
@@ -6,20 +6,26 @@
 </template>
 
 <script setup>
-// item must match the [].vue page name here
-// const { item } = useRoute().params;
-
-// const {item } = defineProps(['item']);
-
-//adding the itemdetails.vue layout to these pages.
-
+let items = [];
 const {item} = useRoute().params;
+
+
+
+const { data } = await useAsyncData('items', () =>
+  $fetch(
+    'https://script.google.com/macros/s/AKfycbylLKlTRlulUb0x9r9j2Wvxa5W64g49NOT9kOsXR-N6LiPRamqA/exec'
+  )
+);
+
+items = data._value.items;
+
 
 const itemz = useNuxtApp().payload.data['items'];
 const myArray = itemz.items;
 
 const myObj = myArray.find((myObj) => myObj.path === item);
 console.log(myObj);
+
 
 
 definePageMeta({
