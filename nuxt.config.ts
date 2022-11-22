66af31aa8cafe4b8bf8b055fdb90b7d3783e5f7f
korbal <balint.j.korosi@gmail.com>
2022-11-22 13:05:16 +0100

    enabled ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
+  // target: 'static',
+  ssr: true,
   modules: ['@nuxtjs/tailwindcss']
   //experimental.payloadExtraction = false
 })
