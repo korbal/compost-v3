df53d4c546aaf1246756555a17c99d5bc02a0dfe
korbal <balint.j.korosi@gmail.com>
2022-12-12 11:13:29 +0100

    deploy: added addthis social share

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <div class="sharethis-inline-share-buttons mb-4"></div>  
-
+  
+    <!-- sharethis.com code -->
+    <!-- <div class="sharethis-inline-share-buttons mb-4"></div>   -->
+    
+    <!-- addthis.com code -->
+    <div class="addthis_inline_share_toolbox container mx-auto flex justify-center my-4"></div>      
     <footer class="container mx-auto px-10 py-4 flex justify-around border-t-2 w-fit">
       <ul class="flex gap-4 text-gray-400">
           <li><NuxtLink to="/" active-class="active">Home</NuxtLink></li>
           <li><NuxtLink to="/about" active-class="active">About</NuxtLink></li>
           <li><NuxtLink to="/items" active-class="active">All Items</NuxtLink></li>
-          
+
+            
       </ul>
 
       
