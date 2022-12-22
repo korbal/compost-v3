756d8f6ae28d43b9928b18a176e629a67e46a243
korbal <balint.j.korosi@gmail.com>
2022-12-22 07:09:55 +0100

    deploy: added full sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
         rel: 'icon',
         type: 'image/png',
         sizes: '32x32',
-        href: '/assets/img/favicon-32x32.png'
+        href: '/favicon.ico'
       }],
 
 
