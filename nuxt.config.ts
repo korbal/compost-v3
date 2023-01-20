7a5a46f40fbaecb714a10bd51cd00caec06a4bc8
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-20 16:36:20 +0100

    deploy: v3.4.0 added buildModules PWA to turn it into a progressive web app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,5 +37,8 @@ export default defineNuxtConfig({
       // link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap' }]
     }
   },
+  buildModules: [
+    '@nuxtjs/pws',
+  ],
   // generate: { routes: ['/items'] },
 })
