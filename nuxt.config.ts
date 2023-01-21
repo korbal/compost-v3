c74c80cb2cd649a18a92452bb54346b2b6e83ec5
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-21 09:29:36 +0100

    deploy: v3.4.1 moving favicon to dist at build time manually again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
       link: [{
         rel: 'icon',
         type: 'image/x-icon',
-        href: '/assets/img/favicon2.ico'
+        href: '/assets/img/favicon.ico'
       }],
 
 
