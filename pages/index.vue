f29df2f02833ed075cee608d3030b1a4a9576607
Balint Korosi <balint.j.korosi@gmail.com>
2023-02-14 12:49:25 +0100

    added quiz

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       <NuxtLink to="/about"
         ><span class="dotted-underline"><br />Learn more.</span></NuxtLink
       >
-      <br />v3.5.7
+      <br />v3.5.8
     </div>
   </div>
 </template>
