45e2a4f4112a9869d343acf34e94a5a7babbe5d6
Balint Korosi <balint.j.korosi@gmail.com>
2023-03-03 06:44:34 +0100

    something

---
+++ server/api/test.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import fs from "fs";
+
+//fucking ghseet persmission. response is html, dunno why. fuck you.
+export default defineEventHandler((event) => {
+  return {
+    items: [],
+  };
+});
