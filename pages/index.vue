e57b4b96d8ed6132f8cd94a104ad7e40b4a56243
Balint Korosi <balint.j.korosi@gmail.com>
2023-04-19 14:53:35 +0200

    deploy: v3.5.15 added dragon fruit

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       <NuxtLink to="/about"
         ><span class="dotted-underline"><br />Learn more.</span></NuxtLink
       >
-      <br />v3.5.14.
+      <br />v3.5.15.
     </div>
   </div>
 </template>
