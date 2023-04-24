945576897bd5933ed65783349f5b2cabac805c15
Balint Korosi <balint.j.korosi@gmail.com>
2023-04-24 19:00:25 +0200

    deploy: v3.5.17 added feathers

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       <NuxtLink to="/about"
         ><span class="dotted-underline"><br />Learn more.</span></NuxtLink
       >
-      <br />v3.5.16.
+      <br />v3.5.17.
     </div>
   </div>
 </template>
