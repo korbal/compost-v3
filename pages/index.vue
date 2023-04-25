2865147642a8b259966b95b5810e1305a15644dc
Balint Korosi <balint.j.korosi@gmail.com>
2023-04-25 10:02:49 +0200

    deploy: v3.5.18 re-push

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       <NuxtLink to="/about"
         ><span class="dotted-underline"><br />Learn more.</span></NuxtLink
       >
-      <br />v3.5.17.
+      <br />v3.5.18.
     </div>
   </div>
 </template>
