7d670ff2dd60a2511d3d221916842c1ec6e2c083
Balint Korosi <balint.j.korosi@gmail.com>
2023-05-08 15:27:54 +0200

    deploy: v3.5.19 coconut milk

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       <NuxtLink to="/about"
         ><span class="dotted-underline"><br />Learn more.</span></NuxtLink
       >
-      <br />v3.5.18.
+      <br />v3.5.19.
     </div>
   </div>
 </template>
