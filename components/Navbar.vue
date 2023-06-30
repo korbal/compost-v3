4df903ca76a42ba2fcb3c88242339d551a86fca6
Balint Korosi <balint.j.korosi@gmail.com>
2023-06-30 07:43:56 +0200

    added guide menu

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,33 +1,27 @@
 <template>
   <div>
-
     <header class="shadow-sm bg-white">
       <nav class="container mx-auto py-4 flex justify-between max-w-2xl">
-      
-      <NuxtLink to="/">
-      <div>
-          <img src="../assets/img/cici-logo.png" alt="" class="logo pl-4" />
-        </div>
-      </NuxtLink>
-
-
-        <ul class="flex gap-4 pr-4">
-          
-          <!-- <li><NuxtLink to="/about">About</NuxtLink></li> -->
-          
-
+        <NuxtLink to="/">
+          <div>
+            <img src="../assets/img/cici-logo.png" alt="" class="logo pl-4" />
+          </div>
+        </NuxtLink>
+        <ul class="flex gap-4 pr-6 font-semibold">
+          <!-- Add the new link -->
+          <li>
+            <a
+              class="hover:underline"
+              href="https://go.canicompostit.com/green-brown"
+              >Guide</a
+            >
+          </li>
         </ul>
       </nav>
     </header>
-
-
   </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
