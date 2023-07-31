6224255079a5876585621de5c704b9550b29641c
Balint Korosi <balint.j.korosi@gmail.com>
2023-07-31 15:41:26 +0200

    very fucking weird things.

--- server/api/test.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import fs from "fs";
-
-//fucking ghseet persmission. response is html, dunno why. fuck you.
-export default defineEventHandler((event) => {
-  return {
-    items: [],
-  };
-});
