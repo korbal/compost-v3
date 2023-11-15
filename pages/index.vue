8250eead00249957976ba27c8305cc28d0905181
Balint Korosi <balint.j.korosi@gmail.com>
2023-11-15 09:01:32 +0100

    deploy: v3.8.6.1 updated sitemap

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       <NuxtLink to="/about"
         ><span class="dotted-underline"><br />Learn more.</span></NuxtLink
       >
-      <br />v3.8.5.10.
+      <br />v3.8.6.1.
     </div>
   </div>
 </template>
