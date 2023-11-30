d5552f2e2e5aadecafcf25f52962eccd4836ecf4
Balint Korosi <balint.j.korosi@gmail.com>
2023-11-30 16:14:09 +0100

    deploy: v3.8.6.2 repush

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       <NuxtLink to="/about"
         ><span class="dotted-underline"><br />Learn more.</span></NuxtLink
       >
-      <br />v3.8.6.2.
+      <br />v3.8.6.2
     </div>
   </div>
 </template>
