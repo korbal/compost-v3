a33233bc4887e1af32dab52fb41e2191f486b764
Balint <balint.j.korosi@gmail.com>
2023-12-12 10:34:58 +0100

    deploy: v3.8.6.12 update 20 items

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       <NuxtLink to="/about"
         ><span class="dotted-underline"><br />Learn more.</span></NuxtLink
       >
-      <br />v3.8.6.11.
+      <br />v3.8.6.12.
     </div>
   </div>
 </template>
